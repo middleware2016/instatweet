4732e7c21ae11536544899c024e74626ab5c6e0c
Alex Delbono <alex.delbono@mail.polimi.it>
2017-02-19 15:13:05 +0100

    [Tweet] Remove userID

--- utility/src/main/java/Tweet.java
+++ utility/src/main/java/Tweet.java
@@ -12,25 +12,29 @@ import java.io.Serializable;
 public class Tweet implements Serializable {
 
 
-    private String publisherUsername;
-    private int publisherID;
-    private ImageIcon content;
+    private final String publisherUsername;
+    private final ImageIcon content;
 
     //When img is a full image this is invalid (-1)
     //When img is a thumbnail this is the id of the full image for the lookup in the database
-    private int fullImgID;
+    private final int fullImgID;
 
-    public Tweet(String publisherUsername, int publisherID, ImageIcon content, int fullImgID) {
+    public Tweet(String publisherUsername, String tweet ,Image img, int fullImgID) {
         this.publisherUsername = publisherUsername;
-        this.publisherID = publisherID;
-        this.content = content;
+        this.content = new ImageIcon(img, tweet);
         this.fullImgID = fullImgID;
+    }
+
+    public Tweet(String publisherUsername, String tweet ,Image img) {
+        this.publisherUsername = publisherUsername;
+        this.content = new ImageIcon(img, tweet);
+        this.fullImgID = -1;
     }
 
     public Tweet(Tweet t){
         publisherUsername = t.publisherUsername;
-        publisherID=t.publisherID;
         content = new ImageIcon(t.content.getImage(), t.content.getDescription());
+        fullImgID=t.fullImgID;
 
     }
 
@@ -38,39 +42,15 @@ public class Tweet implements Serializable {
         return publisherUsername;
     }
 
-    public void setPublisherUsername(String publisherUsername) {
-        this.publisherUsername = publisherUsername;
-    }
-
-    public int getPublisherID() {
-        return publisherID;
-    }
-
-    public void setPublisherID(int publisherID) {
-        this.publisherID = publisherID;
-    }
-
     public Image getImg() {
         return content.getImage();
-    }
-
-    public void setImg(Image img) {
-        content.setImage(img);
     }
 
     public String getText() {
         return content.getDescription();
     }
 
-    public void setText(String text) {
-        content.setDescription(text);
-    }
-
     public int getFullImgID() {
         return fullImgID;
-    }
-
-    public void setFullImgID(int fullImgID) {
-        this.fullImgID = fullImgID;
     }
 }

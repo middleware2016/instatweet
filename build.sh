0b13a45aeb71e8be72a15cf912a6f27aaa8ba984
Alex Delbono <alex.delbono@mail.polimi.it>
2017-02-22 14:50:49 +0100

    Rmi class not found solved

--- build.sh
+++ build.sh
@@ -2,6 +2,3 @@
 
 gradle build
 
-mkdir -p rmicodebase
-cp utility/build/classes/main/interfaces/*Interface.class utility/build/classes/main/payloads/Tweet.class rmicodebase
-
--- runrmi.sh
+++ runrmi.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "Launching rmiregistry..."
-(rmiregistry -J-Djava.rmi.server.codebase=file://$(pwd)/rmicodebase/ &) && echo "rmiregistry launched and running in background."
+(rmiregistry -J-Djava.rmi.server.codebase=file://$(pwd)/utility/build/classes/main/ &) && echo "rmiregistry launched and running in background."
